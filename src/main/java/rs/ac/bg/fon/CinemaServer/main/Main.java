75f9c7dee1e60512bbf38b5ba3b2e6040ad61ed7
nikolaabadic <nikolaabadic@gmail.com>
2021-06-18 22:34:49 +0200

    Main class added.

---
+++ src/main/java/rs/ac/bg/fon/CinemaServer/main/Main.java
@@ -0,0 +1,9 @@
+package rs.ac.bg.fon.CinemaServer.main;
+
+import rs.ac.bg.fon.CinemaServer.form.FrmMain;
+
+public class Main {
+    public static void main(String[] args) {
+        new FrmMain().setVisible(true);
+    }
+}
