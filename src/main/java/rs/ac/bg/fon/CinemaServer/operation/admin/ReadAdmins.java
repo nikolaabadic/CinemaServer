512c14478efb611103ed4b068dbc264ae02360eb
nikolaabadic <nikolaabadic@gmail.com>
2021-06-19 17:07:31 +0200

    Admin related system operations documented.

--- src/main/java/rs/ac/bg/fon/CinemaServer/operation/admin/ReadAdmins.java
+++ src/main/java/rs/ac/bg/fon/CinemaServer/operation/admin/ReadAdmins.java
@@ -6,13 +6,30 @@ import rs.ac.bg.fon.CinemaCommon.domain.Admin;
 import rs.ac.bg.fon.CinemaCommon.domain.GenericEntity;
 import rs.ac.bg.fon.CinemaServer.operation.AbstractGenericOperation;
 
+/**
+ * System operation that returns all admins from the database.
+ * @author Nikola Abadic
+ *
+ */
 public class ReadAdmins extends AbstractGenericOperation {
+	/**
+	 * List of admins that will store the operation result.
+	 */
     private List<GenericEntity> list;
     
+    /**
+     * Returns the list of admins.
+     * @return List of admins.
+     */
     public List<GenericEntity> getList(){
         return list;
     }
     
+    /**
+     * Verifies if the given parameter is an Admin class object.
+     * @param param Object that is sent as request argument.
+     * @throws Exception If the given parameter is null or isn't a Admin class object.
+     */
     @Override
     protected void preconditions(Object param) throws Exception {
         if (param == null || !(param instanceof Admin)) {
@@ -20,6 +37,12 @@ public class ReadAdmins extends AbstractGenericOperation {
         }
     }
 
+
+    /**
+     * Executes the operation by calling the getAll method on the repository attribute.
+     * @param param Object that is sent as request argument.
+     * @throws Exception Thrown by the repository method if there were any errors.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         list = repository.getAll((GenericEntity)param);
