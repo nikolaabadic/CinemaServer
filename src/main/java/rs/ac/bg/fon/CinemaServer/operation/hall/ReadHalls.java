2a00d814f3478f727a1174730963bb1fec241448
nikolaabadic <nikolaabadic@gmail.com>
2021-06-19 17:26:29 +0200

    Hall related system operations documented.

--- src/main/java/rs/ac/bg/fon/CinemaServer/operation/hall/ReadHalls.java
+++ src/main/java/rs/ac/bg/fon/CinemaServer/operation/hall/ReadHalls.java
@@ -6,13 +6,30 @@ import rs.ac.bg.fon.CinemaCommon.domain.GenericEntity;
 import rs.ac.bg.fon.CinemaCommon.domain.Hall;
 import rs.ac.bg.fon.CinemaServer.operation.AbstractGenericOperation;
 
+/**
+ * System operation that returns all halls from the database.
+ * @author Nikola Abadic
+ *
+ */
 public class ReadHalls extends AbstractGenericOperation{
-	   private List<GenericEntity> list;
+	    /**
+	    * List of halls that will store the operation result.
+	    */
+	    private List<GenericEntity> list;
 	    
+	    /**
+	     * Returns the list of halls.
+	     * @return List of halls.
+	     */
 	    public List<GenericEntity> getList(){
 	        return list;
 	    }
 	    
+	    /**
+	     * Verifies if the given parameter is not null and is a Hall class object.
+	     * @param param Object that is sent as request argument.
+	     * @throws Exception If the given parameter is null or isn't a Hall class object.
+	     */
 	    @Override
 	    protected void preconditions(Object param) throws Exception {
 	        if (param == null || !(param instanceof Hall)) {
@@ -20,6 +37,11 @@ public class ReadHalls extends AbstractGenericOperation{
 	        }
 	    }
 
+	    /**
+	     * Executes the operation by calling the getAll method on the repository attribute.
+	     * @param param Object that is sent as request argument.
+	     * @throws Exception Thrown by the repository method if there were any errors.
+	     */
 	    @Override
 	    protected void executeOperation(Object param) throws Exception {
 	        list = repository.getAll((Hall)param);
--- src/main/java/rs/ac/bg/fon/CinemaServer/operation/hall/ReadHallsWhere.java
+++ src/main/java/rs/ac/bg/fon/CinemaServer/operation/hall/ReadHallsWhere.java
@@ -6,18 +6,42 @@ import rs.ac.bg.fon.CinemaCommon.domain.GenericEntity;
 import rs.ac.bg.fon.CinemaCommon.domain.Hall;
 import rs.ac.bg.fon.CinemaServer.operation.AbstractGenericOperation;
 
+/**
+ * System operation that returns all halls from the database which satisfies a particular condition.
+ * @author Nikola Abadic
+ *
+ */
 public class ReadHallsWhere extends AbstractGenericOperation{
+	/**
+	 * List of halls that will store the operation result.
+	 */
     private List<GenericEntity> list;
+    /**
+     * String which represents the SQL where condition.
+     */
     private String where;
 
+    /**
+     * Parameterized constructor that initializes a ReadHallsWhere object and sets the where attribute value.
+     * @param where String which represents the SQL where condition.
+     */
     public ReadHallsWhere(String where) {
         this.where = where;
     }
         
+    /**
+     * Returns the list of halls.
+     * @return List of halls.
+     */
     public List<GenericEntity> getList(){
         return list;
     }
     
+    /**
+     * Verifies if the given parameter is not null and is a Hall class object.
+     * @param param Object that is sent as request argument.
+     * @throws Exception If the given parameter is null or isn't a Hall class object.
+     */
     @Override
     protected void preconditions(Object param) throws Exception {
         if (param == null || !(param instanceof Hall)) {
@@ -25,6 +49,11 @@ public class ReadHallsWhere extends AbstractGenericOperation{
         }
     }
 
+    /**
+     * Executes the operation by calling the getAll method on the repository attribute.
+     * @param param Object that is sent as request argument.
+     * @throws Exception Thrown by the repository method if there were any errors.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         list = repository.getAll((Hall)param,where);
