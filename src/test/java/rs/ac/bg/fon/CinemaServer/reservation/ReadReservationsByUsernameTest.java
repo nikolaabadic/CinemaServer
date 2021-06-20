862644f50b6b6285b1d036bbc8e14758fec118ea
nikolaabadic <nikolaabadic@gmail.com>
2021-06-20 17:30:12 +0200

    ReadReservationsByUsername system operation tested.

---
+++ src/test/java/rs/ac/bg/fon/CinemaServer/reservation/ReadReservationsByUsernameTest.java
@@ -0,0 +1,132 @@
+package rs.ac.bg.fon.CinemaServer.reservation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import rs.ac.bg.fon.CinemaCommon.domain.Film;
+import rs.ac.bg.fon.CinemaCommon.domain.GenericEntity;
+import rs.ac.bg.fon.CinemaCommon.domain.Reservation;
+import rs.ac.bg.fon.CinemaCommon.domain.Term;
+import rs.ac.bg.fon.CinemaCommon.domain.User;
+import rs.ac.bg.fon.CinemaServer.operation.AbstractGenericOperation;
+import rs.ac.bg.fon.CinemaServer.operation.reservation.CreateReservation;
+import rs.ac.bg.fon.CinemaServer.operation.reservation.DeleteReservation;
+import rs.ac.bg.fon.CinemaServer.operation.reservation.ReadReservationsByUsername;
+import rs.ac.bg.fon.CinemaServer.operation.terms.ReadTerms;
+import rs.ac.bg.fon.CinemaServer.operation.user.ReadUsers;
+
+public class ReadReservationsByUsernameTest {
+	private AbstractGenericOperation readByUsername;
+	private static User user;
+	private static Term term;
+	private static Reservation createdReservation;
+	
+	@BeforeAll
+	static void start() throws Exception {
+		FileOutputStream out = new FileOutputStream("config/dbconfig.properties");
+		Properties properties = new Properties();
+        properties.setProperty("url","jdbc:mysql://localhost:3306/cinema-test");
+        properties.setProperty("username", "root");
+        properties.setProperty("password", "root");
+        properties.store(out, null);
+        out.close();        
+        
+        AbstractGenericOperation readTerms = new ReadTerms();
+        readTerms.execute(new Term());
+        term = (Term)((ReadTerms)readTerms).getList().get(0);
+        
+        AbstractGenericOperation readUsers = new ReadUsers();
+        readUsers.execute(new User());
+        user = (User)((ReadUsers)readUsers).getList().get(0);
+	}
+	
+	@AfterAll
+	static void end() throws Exception{
+		AbstractGenericOperation deleteReservation = new DeleteReservation();
+		deleteReservation.execute(createdReservation);
+		
+		FileOutputStream out = new FileOutputStream("config/dbconfig.properties");
+		Properties properties = new Properties();
+        properties.setProperty("url", "jdbc:mysql://localhost:3306/cinema");
+        properties.setProperty("username", "root");
+        properties.setProperty("password", "root");
+        properties.store(out, null);
+        out.close();
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		readByUsername = new ReadReservationsByUsername("");
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception{
+		readByUsername = null;
+	}
+	
+	@Test
+	void constructorTest() {
+		readByUsername= new ReadReservationsByUsername("");
+		assertNotNull(readByUsername);
+	}
+	
+	@Test
+	void testPreconditionsNull() {
+		assertThrows(java.lang.Exception.class, () -> readByUsername.execute(null));
+	}
+	
+	@Test
+	void testPreconditionsInvalidData() {
+		assertThrows(java.lang.Exception.class, () -> readByUsername.execute(new Film()));
+	}
+	
+	@Test
+	void testIncorrectWhereString() {
+		readByUsername = new ReadReservationsByUsername("LOWER(u.username) LIKKE '%user%' ");
+		assertThrows(java.lang.Exception.class, () -> readByUsername.execute(new Reservation()));
+	}
+	
+	@Test
+	void testReadReservations() throws Exception {
+		Reservation reservation = new Reservation();
+		Date date = new Date();
+		reservation.setNumberOfTickets(1);
+		reservation.setReservationDate(date);
+		reservation.setUser(user);
+		reservation.setTerm(term);
+		
+		AbstractGenericOperation createReservation = new CreateReservation();
+		createReservation.execute(reservation);
+		
+		readByUsername = new ReadReservationsByUsername("LOWER(u.username) LIKE '%user%' ");
+		readByUsername.execute(new Reservation());
+		List<GenericEntity> reservations = ((ReadReservationsByUsername)readByUsername).getList();
+		
+		assertNotNull(reservations);
+		assertEquals(1, reservations.size());
+		
+		createdReservation = (Reservation)reservations.get(0);
+		assertEquals(reservation.getUser().getUserID(), createdReservation.getUser().getUserID());
+		assertEquals(reservation.getTerm().getTermID(), createdReservation.getTerm().getTermID());
+		assertEquals(reservation.getNumberOfTickets(), createdReservation.getNumberOfTickets());
+	
+		SimpleDateFormat sdf =  new SimpleDateFormat("dd.MM.yyyy HH:mm");
+		String d1 = sdf.format(date);
+		String d2 = sdf.format(createdReservation.getReservationDate());
+		assertEquals(d1, d2);
+	}
+	
+}
